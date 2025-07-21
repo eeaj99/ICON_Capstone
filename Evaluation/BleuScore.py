393511a4be101d8c98b39c2dec7a37a3a0bd4786
nivish10 <nidhi.v.sharma@ucdconnect.ie>
2025-07-22 00:11:06 +0100

    Evaluation files added

---
+++ Evaluation/BleuScore.py
@@ -0,0 +1,24 @@
+import json
+import nltk
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt_tab')
+# Load JSON data
+with open("documents\qa_pairs.json", "r") as f:
+    data = json.load(f)
+
+smooth = SmoothingFunction().method1
+bleu_scores = []
+
+# Loop through each QA pair
+for entry in data:
+    reference = word_tokenize(entry['standard_answer'].lower())
+    hypothesis = word_tokenize(entry['chatbot_answer'].lower())
+
+    score = sentence_bleu([reference], hypothesis, smoothing_function=smooth)
+    bleu_scores.append(score)
+
+# Average BLEU score
+average_bleu = sum(bleu_scores) / len(bleu_scores)
+print(f"Average BLEU Score: {average_bleu:.4f}")
